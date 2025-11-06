14dfa46f2dfda744bbf3ae5a07a7a0fb4228dfc0
Muhammad_Egb <mhmad.m.ig2001@gmail.com>
2025-11-07 01:23:08 +0200

    Add signup with JWT, refresh token storage, and cookies

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from "express"
-import {signup,login,loguot} from "../controllers/auth.controller.js"
+import {signup,login,logout } from "../controllers/auth.controller.js"
 const router = express.Router();
 
-router.get("/signup",signup);
-router.get("/login",login)
-router.get("/logout",loguot)
+// router.get("/signup",signup);
+// router.get("/login",login)
+// router.get("/logout",loguot)
 
-export default router
+router.post("/signup",signup);
+router.post("/login",login)
+router.post("/logout",logout )
 
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000
 
+app.use(express.json()); // allows you to parse the body of the request 
+
 app.use("/api/auth",authRoutes);
 
 app.listen(PORT,() => {
@@ -15,4 +17,3 @@ app.listen(PORT,() => {
 
     connectDB();
 })
-ASD ZX
